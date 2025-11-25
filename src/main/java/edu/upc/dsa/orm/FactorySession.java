1caead5e6f96ce2443a498981be1eec219a6a81e
JairoLopezCarbo <jairo.lopez@estudiantat.upc.edu>
2025-11-25 12:08:38 +0100

    Mariadbdump

--- src/main/java/edu/upc/dsa/orm/FactorySession.java
+++ src/main/java/edu/upc/dsa/orm/FactorySession.java
@@ -19,7 +19,7 @@ public class FactorySession {
         String host = "127.0.0.1";
         String port = "3306";
         String user = "root";
-        String pass = "4282";
+        String pass = "root";
 
 
         Connection connection = null;
